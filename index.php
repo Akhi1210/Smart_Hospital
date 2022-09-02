799740104589ac3fc551808230edb8f9a418ef14
Akhi1210 <112709743+Akhi1210@users.noreply.github.com>
2022-09-03 00:27:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-    header("location:doctors_UI.php");
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href='patient_login_doctor.php'>patient login @doctor</a><br><br>
+    <a href='patient_login_pharmacy.php'>patient login @pharmacy</a><br><br>
+    <a href='doctors_UI.php'>Doctors UI</a><br><br>
+    <a href='pharmacy_UI.php'>Pharmacy UI</a><br><br>
+    <a href='doctor_login'>Doctor login page</a><br><br>
+    <a href='pharmacist_login.php'>Pharmacy login page</a><br><br>
+    <a href='aadhar_entry.php'>Aahdar entry</a><br>
+    
+    
+</body>
+</html>
