9a19b574279855cac156e65fc8079089d7cfc74e
Akhi1210 <112709743+Akhi1210@users.noreply.github.com>
2022-09-03 01:18:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,14 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <a href='patient_login_doctor.php' target='_blank'>patient login @doctor</a><br><br>
-    <a href='patient_login_pharmacy.php' target='_blank'>patient login @pharmacy</a><br><br>
-    <a href='doctors_UI.php' target='_blank'>Doctors UI</a><br><br>
-    <a href='pharmacy_UI.php' target='_blank'>Pharmacy UI</a><br><br>
-    <a href='doctor_login.php' target='_blank'>Doctor login page</a><br><br>
-    <a href='pharmacist_login.php' target='_blank'>Pharmacy login page</a><br><br>
-    <a href='aadhar_entry.php' target='_blank'>Aahdar entry</a><br>
+<body style='margin-left:100px;background-color:azure'>
+    <div>
+        <h1 style='padding-left:20px;'>Main Menu</h1>
+        <a href='patient_login_doctor.php' target='_blank'>patient login @doctor</a><br><br>
+        <a href='patient_login_pharmacy.php' target='_blank'>patient login @pharmacy</a><br><br>
+        <a href='doctors_UI.php' target='_blank'>Doctors UI</a><br><br>
+        <a href='pharmacy_UI.php' target='_blank'>Pharmacy UI</a><br><br>
+        <a href='doctor_login.php' target='_blank'>Doctor login page</a><br><br>
+        <a href='pharmacist_login.php' target='_blank'>Pharmacy login page</a><br><br>
+        <a href='aadhar_entry.php' target='_blank'>Aahdar entry</a><br>
+    </div>
     
     
 </body>
