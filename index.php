eb55f83e17b11f3bf763004f48b7a8e0581da7da
Akhi1210 <112709743+Akhi1210@users.noreply.github.com>
2022-09-03 01:13:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
     <title>Document</title>
 </head>
 <body>
-    <a href='patient_login_doctor.php'>patient login @doctor</a><br><br>
-    <a href='patient_login_pharmacy.php'>patient login @pharmacy</a><br><br>
-    <a href='doctors_UI.php'>Doctors UI</a><br><br>
-    <a href='pharmacy_UI.php'>Pharmacy UI</a><br><br>
-    <a href='doctor_login.php'>Doctor login page</a><br><br>
-    <a href='pharmacist_login.php'>Pharmacy login page</a><br><br>
-    <a href='aadhar_entry.php'>Aahdar entry</a><br>
+    <a href='patient_login_doctor.php' target='_blank'>patient login @doctor</a><br><br>
+    <a href='patient_login_pharmacy.php' target='_blank'>patient login @pharmacy</a><br><br>
+    <a href='doctors_UI.php' target='_blank'>Doctors UI</a><br><br>
+    <a href='pharmacy_UI.php' target='_blank'>Pharmacy UI</a><br><br>
+    <a href='doctor_login.php' target='_blank'>Doctor login page</a><br><br>
+    <a href='pharmacist_login.php' target='_blank'>Pharmacy login page</a><br><br>
+    <a href='aadhar_entry.php' target='_blank'>Aahdar entry</a><br>
     
     
 </body>
