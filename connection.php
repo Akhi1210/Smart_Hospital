fd60c89205724a693175470afe071d98d0b17671
Akhi1210 <112709743+Akhi1210@users.noreply.github.com>
2022-09-03 10:05:22 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,10 +1,12 @@
 <?php
-$db_server_name = "ec2-44-207-126-176.compute-1.amazonaws.com";
+$db_server_name = "host = ec2-44-207-126-176.compute-1.amazonaws.com";
 $user_name = "wqujkwoxfevzar";
 $password = "eebf86c32af3c380deadd3de8a0e016c9af31e6714985169d9a49f48272bc969";
-$db_name = "dbtv8t53sqhdco";
+$db_name = "dbname = dbtv8t53sqhdco";
+$credentials = "user = wqujkwoxfevzar password=eebf86c32af3c380deadd3de8a0e016c9af31e6714985169d9a49f48272bc969";
+$port = "port = 5432"
 date_default_timezone_set('Asia/Kolkata');
-if (!$conn = mysqli_connect($db_server_name,$user_name,$password,$db_name))
+if (!$conn = pg_connect("$db_server_name $port $db_name $credentials")
     {
     die("Failed to Connect!");
     }
