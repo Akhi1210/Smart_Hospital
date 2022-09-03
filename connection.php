b304aef21f2ea94bbefaf940d68a9a832f359957
Akhi1210 <112709743+Akhi1210@users.noreply.github.com>
2022-09-03 09:28:26 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$db_server_name = "localhost";
-$user_name = "root";
-$password = "Truth@66567";
-$db_name = "e_hosp_php";
+$db_server_name = "ec2-44-207-126-176.compute-1.amazonaws.com:5432";
+$user_name = "wqujkwoxfevzar";
+$password = "eebf86c32af3c380deadd3de8a0e016c9af31e6714985169d9a49f48272bc969";
+$db_name = "dbtv8t53sqhdco";
 date_default_timezone_set('Asia/Kolkata');
 if (!$conn = mysqli_connect($db_server_name,$user_name,$password,$db_name))
     {
