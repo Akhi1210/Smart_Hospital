0ae7c5a62fc340a6601113e511e90b4e1e91cb7c
Akhi1210 <112709743+Akhi1210@users.noreply.github.com>
2022-09-03 09:52:30 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-/*$db_server_name = "ec2-44-207-126-176.compute-1.amazonaws.com:5432";
+$db_server_name = "ec2-44-207-126-176.compute-1.amazonaws.com";
 $user_name = "wqujkwoxfevzar";
 $password = "eebf86c32af3c380deadd3de8a0e016c9af31e6714985169d9a49f48272bc969";
 $db_name = "dbtv8t53sqhdco";
@@ -7,6 +7,6 @@ date_default_timezone_set('Asia/Kolkata');
 if (!$conn = mysqli_connect($db_server_name,$user_name,$password,$db_name))
     {
     die("Failed to Connect!");
-    }*/
+    }
 echo "This is connection.php";
 ?>
