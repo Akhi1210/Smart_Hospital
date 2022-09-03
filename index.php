a5edd2fa67ff75e3fe14c25204aeb0b7db2f1b51
Akhi1210 <112709743+Akhi1210@users.noreply.github.com>
2022-09-03 12:16:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,18 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='styles_e_hospital.css'>
     <title>Document</title>
 </head>
-<body style='margin-left:10px;background-color:azure'>
+<body class='box_plp' style="background-image:url('/background_images/index.jpeg');background-size: cover">
     <div>
-        <h1 style='padding-left:10px;'>Main Menu</h1>
-        <a href='patient_login_doctor.php' target='_blank'>patient login @doctor</a><br><br>
-        <a href='patient_login_pharmacy.php' target='_blank'>patient login @pharmacy</a><br><br>
-        <a href='doctors_UI.php' target='_blank'>Doctors UI</a><br><br>
-        <a href='pharmacy_UI.php' target='_blank'>Pharmacy UI</a><br><br>
-        <a href='doctor_login.php' target='_blank'>Doctor login page</a><br><br>
-        <a href='pharmacist_login.php' target='_blank'>Pharmacy login page</a><br><br>
-        <a href='aadhar_entry.php' target='_blank'>Aahdar entry</a><br>
+    <h2 style='padding-left:10px'>Main Menu</h2>
+    <a href='patient_login_doctor.php' style='color:white' target='_blank'>patient login @doctor</a><br><br>
+    <a href='patient_login_pharmacy.php' style='color:white' target='_blank'>patient login @pharmacy</a><br><br>
+    <a href='doctors_UI.php' style='color:white' target='_blank'>Doctors UI</a><br><br>
+    <a href='pharmacy_UI.php' style='color:white' target='_blank'>Pharmacy UI</a><br><br>
+    <a href='doctor_login.php' style='color:white' target='_blank'>Doctor login page</a><br><br>
+    <a href='pharmacist_login.php' style='color:white' target='_blank'>Pharmacy login page</a><br><br>
+    <a href='aadhar_entry.php' style='color:white' target='_blank'>Aadhar entry</a><br>
     </div>
     
     
