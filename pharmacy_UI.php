da85136e822b1bb4864561c3381f1fc9ada2812e
Akhi1210 <112709743+Akhi1210@users.noreply.github.com>
2022-09-04 12:13:19 +0530

    Update pharmacy_UI.php

--- pharmacy_UI.php
+++ pharmacy_UI.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pharmacy</title>
-    <link rel='icon' href='/icons/pharmacy.ico'/>
+    <link rel='icon' href='/icons/Pharmacy.ico'/>
     <link rel='stylesheet' href='styles_e_hospital.css'/>
 </head>
 <body class='box_pp' style="background-image:url('/background_images/pharm_ui.jpeg');background-size: cover">
