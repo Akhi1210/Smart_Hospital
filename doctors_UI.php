3863366a2f317adad0ff229aaa669a5749ae781d
Akhi1210 <112709743+Akhi1210@users.noreply.github.com>
2022-09-04 11:45:48 +0530

    Update doctors_UI.php

--- doctors_UI.php
+++ doctors_UI.php
@@ -5,21 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Doctor</title>
-    <link rel='icon' href='/icons/Doctor.ico'/>
+    <link rel='icon' href='/icons/doctor.ico'/>
 
     <link rel="stylesheet" href='styles_e_hospital.css' />
         
 
 </head>
-<div style="font-size:30px;font-weight:bold;">Doctor's Page</div>
+
 <body class='box_dp' style="background-image:url('/background_images/doc_ui.jpg');background-size: cover">
->
+
     
     <?php
 
         $var_logout = "<form method='post'>
                             Hi Doctor ! Nice to see you again.
-                            <input class='logout_btn' type='submit' name='logout' value='Logout'>
+                            <input class='logout_btn' style='margin-left:100px' type='submit' name='logout' value='Logout'>
                             </form>";
         echo "$var_logout";
     
@@ -34,16 +34,16 @@
                             <div style='padding-right:40px;display:inline'>
                                 <div style='font-weight:bold;display:inline;'>DOB :</div> 'DOB here'
                              </div>
-                            <div style='font-weight:bold;display:inline;'>Gender :</div> 'Gender here'<br>
+                            <div style='margin-left:40px;font-weight:bold;display:inline;'>Gender :</div> 'Gender here'<br>
                           </div>
                         ";
         $var_add_med = "<form method='post'>
                         <div style=' padding-bottom:5px;'>Problem Description:</div>
-                        <textarea id='description' name='description' rows='4' cols='50'>'problem description here'</textarea><br>
+                        <textarea id='description' name='description' rows='4' cols='55'>'problem description here'</textarea><br><br>
                         Medicine: <input type='text' name='med_name' placeholder='Medicine'>
                         Quantity: <input style='width:30px' type='number' name='med_quan'>
                         <input style='height:27px;background-color:lightblue;border-radius: 5px;border-color:white;' type='submit' name='add-btn' value='Add'><br>
-                        </form>";
+                        </form><br>";
                 
 
         $var_save_exit = "<form method='post'>
@@ -56,20 +56,19 @@
 
 
         if(isset($_POST['add-btn'])){
-            $_SESSION['add-strt'] = 1;          //This variable is used for logout condition;And to know existing info;
-            $_SESSION['prolm_description'] = $_POST['description'];
+            
             $med_name = $_POST['med_name'];
             $med_quan = $_POST['med_quan'];
-            try{
-                if ($med_name=='' or $med_quan==0){
-                    throw new Exception('med_name cannot be null');
-                }
-                mysqli_query($conn,$qryd3);}
-            catch (Exception $e){
+            if ($med_name=='' or $med_quan==0){
                 echo "<script>alert('Invalid data');</script>";
+                
             }
-            unset($_POST['add-btn']);
-            header("location:doctors_UI.php");
+            else{
+                unset($_POST['add-btn']);
+                echo "<script>alert('Added to data base');</script>";
+                
+            }
+            
         }
 
         // Prescription Table starts;
