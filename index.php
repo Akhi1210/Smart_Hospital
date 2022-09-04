2c390735c16fc5c751ba667a2763fb547d10f835
Akhi1210 <112709743+Akhi1210@users.noreply.github.com>
2022-09-04 11:47:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,20 +4,57 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='icon' href='/icons/home.png'/>
     <link rel='stylesheet' href='styles_e_hospital.css'>
-    <title>Document</title>
+    <title>Smart Hospital</title>
+
 </head>
-<body class='box_plp' style="background-image:url('/background_images/index.jpeg');background-size: cover">
-    <div>
-    <h2 style='padding-left:10px'>Main Menu</h2>
-    <a href='patient_login_doctor.php' style='color:white' target='_blank'>patient login @doctor</a><br><br>
-    <a href='patient_login_pharmacy.php' style='color:white' target='_blank'>patient login @pharmacy</a><br><br>
-    <a href='doctors_UI.php' style='color:white' target='_blank'>Doctors UI</a><br><br>
-    <a href='pharmacy_UI.php' style='color:white' target='_blank'>Pharmacy UI</a><br><br>
-    <a href='doctor_login.php' style='color:white' target='_blank'>Doctor login page</a><br><br>
-    <a href='pharmacist_login.php' style='color:white' target='_blank'>Pharmacy login page</a><br><br>
-    <a href='aadhar_entry.php' style='color:white' target='_blank'>Aadhar entry</a><br>
+<body class='box_plp' style="background-image:url('/background_images/index.jpeg');background-size: cover;">
+  <h1 style='margin-left:10px;'>SMART HOSPITAL</h1>
+
+  <div class='dropdown'>
+    <button class='dropbtn' title='If you are a doctor navigate from here'>Doctors</button>
+    <div class='dropdown-content'>
+        <a href="doctors_UI.php" target='_blank' title='Go to Doctors user interface'>Doctors_UI</a>
+        <a href="doctor_login.php" target='_blank' title='Login as Doctor'>Doctor_login</a>
     </div>
+    
+</div>
+<div class='dropdown'>
+<button class='dropbtn' title='If you are a pharmacist navigate from here'>Pharmacy</button>
+    <div class='dropdown-content'>
+        <a href="pharmacy_UI.php" target='_blank' title='Go to Pharmacist user interface'>Pharmacists_UI</a>
+        <a href="pharmacist_login.php" target='_blank' title='Login as Pharmacist'>Pharmacist_login</a>
+    </div>  
+</div>
+<div class='dropdown'>
+    <button class='dropbtn' title='If you are a patient navigate from here'>Patients</button>
+    <div class='dropdown-content'>
+        <a href="patient_login_doctor.php" target='_blank' title='Login as patient at doctor'>Login to doctor</a>
+        <a href="patient_login_pharmacy.php" target='_blank' title='Login as Patient at pharmacy'>Login to pharmacy</a>
+        <a href="aadhar_entry.php" target='_blank' title='Submit aadhar details'>Aadhar form</a>
+    </div>
+</div>
+
+<div>
+    <ul style='font-size:20px;'> 
+      <li>Smart hospitals provide an intelligent ecosystem for patients to heal and for staff to work.</li><br>
+      <li> Enhance transparency.</li><br>
+      <li>Avoid mistakes.</li><br>
+      <li>Satisfy patients.</li>
+</ul> 
+  <h4 style='margin-left:10px'>Digital transformation is the key</h1>
+  <ul>
+    <li>Digitalising the prescription makes it simpler</li><br>
+    <li>Keeps track of every treatment.</li><br>
+    <li>Keeps track of every medicine.</li><br>
+    <li>Keeps track of every working minute of doctor and pharmacist</li>
+
+  </ul>
+</div>
+
+
+</body>
     
     
 </body>
